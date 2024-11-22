8469a1c3f21febdff18212c4594fd683dc8be474
SathiyaKumar MURALI <153432871+SathiyaKumarMURALI@users.noreply.github.com>
2024-11-22 09:19:47 +0530

    Add files via upload

---
+++ Landing page Askmeidentity/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      fontFamily:{
+        righteous:['Righteous','cursive'],
+        saira:['saira','Cursive'],
+      },
+    },
+  },
+  plugins: [],
+};
+
